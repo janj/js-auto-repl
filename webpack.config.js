c2a8f5dc44e369262062c03a523e7f9ca65432f5
Jan <jan.jirout@indiegogo.com>
2018-05-15 14:17:41 -0400

    using webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+	output: {
+    path: path.resolve(__dirname, 'dist'),
+	publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+	  rules: [
+		  {
+			  test: /\.vue$/,
+			  loader: 'vue-loader',
+			  options: { loaders: {} }
+		  },
+		  {
+			  test: /\.js$/,
+			  loader: 'babel-loader',
+			  exclude: /(node_modules|bower_components)/,
+			  
+		  }
+	  ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+}
