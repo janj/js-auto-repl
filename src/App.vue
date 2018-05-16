16157ae388cb3c2597f8d5519674a49df8e4f27b
Jan <jan.jirout@indiegogo.com>
2018-05-16 16:08:57 -0700

    PigLatin single file component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
 	<div class="contentArea">
 		<div class="message">{{ msg }}</div>
-		<div>
-			<input class="form-control" v-model="message"></input>
-			<br>
-			<p>{{pigLatin}}</p>
-		</div>
+		<pig-latin></pig-latin>
 	</div>
 </template>
 
 <script>
-	import { map } from 'lodash';    
+	import { map } from 'lodash';
+	import PigLatin from './PigLatin.vue'
 
 	export default {
 		data () {
@@ -19,43 +16,14 @@
 				message: 'Type here, pig latin below.'
 			}
 		},
-		computed: {
-			pigLatin: function () {
-				return this.pigLatinWords(this.message);
-			}
+		components: {
+			PigLatin
 		},
-		methods: {
-			pigLatinWord: function (word) {
-				const capitalizeFirst = (string) => {
-					return string.slice(0, 1).toUpperCase() + string.slice(1);
-				}
-
-				const wasCapitalized = word.slice(0, 1) === word.slice(0, 1).toUpperCase();
-				const regResult = /(\w*)(.*)/.exec(word);
-				const cleanWord = regResult[1];
-				const punctuation = regResult[2];
-
-				const first = cleanWord.slice(0,1).toLowerCase();
-				let rest = cleanWord.slice(1);
-				if (wasCapitalized) {
-					rest = capitalizeFirst(rest);
-				}
-				return `${rest}${first}ay${punctuation}`;
-			},
-			pigLatinWords: function (words) {
-				const wordArray = words.split(" ");
-				const pigLatined = map(wordArray, this.pigLatinWord);
-				return pigLatined.join(" ");
-			}
-		}
 	}
 </script>
 
 <style>
 	.message {
 		color: blue;
-	}
-	.contentArea {
-		padding: 25px;
 	}
 </style>
