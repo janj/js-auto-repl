c5d07bc827160ba079a9be7b3106e5efd958aae5
Jan <jan.jirout@indiegogo.com>
2018-05-16 18:16:42 -0700

    tab vs space fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
+ 	entry: './src/main.js',
 	output: {
-    path: path.resolve(__dirname, 'dist'),
-	publicPath: '/dist/',
-    filename: 'build.js'
-  },
-  module: {
-	  rules: [
-		  {
-			  test: /\.vue$/,
-			  loader: 'vue-loader',
-			  options: { loaders: {} }
-		  },
-		  {
-			  test: /\.js$/,
-			  loader: 'babel-loader',
-			  exclude: /(node_modules|bower_components)/,
-			  
-		  }
-	  ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.common.js'
-    }
-  },
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/dist/',
+		filename: 'build.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: { loaders: {} }
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /(node_modules|bower_components)/,
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			'vue$': 'vue/dist/vue.common.js'
+		}
+	},
 }
